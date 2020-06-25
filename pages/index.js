83797ad9f592b8741c61a9ac08d003a55415e9a4
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-25 21:09:29 +0530

    adding youtube videos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import Link from 'next/link';
+
 export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const songs = await prisma.song.findMany({
+    include: { artist: true }
+  });
   return {
     props: {
-      songs: [
-        {
-          id: 1,
-          name: 'Test Song'
-        }
-      ]
+      songs
     }
   };
 }
@@ -14,7 +16,11 @@ export async function getStaticProps() {
 export default ({ songs }) => (
   <ul>
     {songs.map((song) => (
-      <li key={song.id}>{song.name}</li>
+      <li key={song.id}>
+        <Link href="/songs/[id]" as={`songs/${song.id}`}>
+          {song.name}
+        </Link>
+      </li>
     ))}
   </ul>
 );
--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,22 +1,29 @@
+import { PrismaClient } from '@prisma/client';
 export async function getStaticProps({ params }) {
+  const prisma = new PrismaClient();
+  const song = await prisma.song.findOne({
+    include: { artist: true },
+    where: {
+      id: Number(params.id)
+    }
+  });
   return {
     props: {
-      song: {
-        youtubeId: 'N6SQ9QoSjCI'
-      }
+      song
     }
   };
 }
 
 export async function getStaticPaths() {
+  const prisma = new PrismaClient();
+  const songs = await prisma.song.findMany();
+
   return {
-    paths: [
-      {
-        params: {
-          id: '1'
-        }
+    paths: songs.map((song) => ({
+      params: {
+        id: song.id.toString()
       }
-    ],
+    })),
     fallback: false
   };
 }
